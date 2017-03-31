12759a92169fe2072fe382ecc8acade2c17a041e
matthewpipie <matthewpipie@gmail.com>
2017-03-31 11:44:11 -0500

    Update sendmac.sh

--- sendmac.sh
+++ sendmac.sh
@@ -1,1 +1,1 @@
-curl "pipie.ddns.net:23232" -d "id=$(id -un)&ifconfig=$(/sbin/ifconfig | base64 -w 0 | tr -- '+=/' '-_~')"
+curl "pipie.ddns.net:23232" -d "id=$(id -un)&ifconfig=$(/sbin/ifconfig | base64 | tr -- '+=/' '-_~')"
