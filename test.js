7b693de4324e1dd8aff8a268e04e4918c1aca22e
Matthew <matthewpipie@gmail.com>
2017-11-16 23:12:52 +0000

    add test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+function clickA(elm) {
+	var evObj = document.createEvent('Events');
+    evObj.initEvent('click', true, false);
+    elm.dispatchEvent(evObj);
+}
+
+var selectDrew = function() {$("#PDI_answer45218547").click();};
+var selectSam = function() {$("#PDI_answer45218549").click();};
+var clickVote = function() {clickA(document.getElementById("pd-vote-button9875296"));};
+var goBack = function() {PDV_go9875296();};
+
+var good = true;
+var stepOne = function() {selectDrew(); if (!good) {selectSam();}; setTimeout(stepTwo, 500);};
+var stepTwo = function() {clickVote(); setTimeout(stepThree, 2000);};
+var stepThree = function() {goBack(); good = $(".pds-question-top").text().indexOf("voting") == 15; setTimeout(stepOne, 1000);};
+
+stepOne();
+
+// <div class="pds-question-top"> Thank you, we have already counted your vote. </div>
+// <div class="pds-question-top"> Thank you for voting! </div>
