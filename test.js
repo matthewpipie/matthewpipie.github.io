7e818c4c66fa2241954533a19775d1595418422d
Matthew <matthewpipie@gmail.com>
2017-11-17 00:11:08 +0000

    fix

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ var goBack = function() {PDV_go9875296();};
 var good = true;
 var stepOne = function() {selectDrew(); if (!good) {selectSam();}; setTimeout(stepTwo, 500);};
 var stepTwo = function() {clickVote(); setTimeout(stepThree, 1500);};
-var stepThree = function() {good = $(".pds-question-top").text().indexOf("voting") == 15; goBack(); getTimeout(stepOne, 500);};
+var stepThree = function() {good = $(".pds-question-top").text().indexOf("voting") == 15; goBack(); setTimeout(stepOne, 500);};
 
 stepOne();
 
